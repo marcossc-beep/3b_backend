355d608a2aae6745b0d9c81b523ab6656eddd25e
marcossc-beep <marcos.sc@edu.sc.senai.br>
2026-02-25 09:46:16 -0300

    Remove receitas functionality from server
    Removed receitas endpoints and related code.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import Fastify from 'fastify'
 const servidor = Fastify()
 
 const usuarios = []
-const receitas = []
-
 
 servidor.get('/usuarios', () => {
     return usuarios
@@ -14,21 +12,6 @@ servidor.post('/usuarios', (request, reply) => {
     const body = request.body;
     usuarios.push(body)
     return 'Usuario cadastrado'
-})
-
-servidor.post('/receitas', (request, reply) => {
-    const body = request.body;
-    receitas.push(body)
-    
-    reply.status(200).send({msg: 'Usuario Cadastrado'})
-})
-
-servidor.put('/receitas/:titulo', (request, reply) => {
-    return request.params.titulo
-})
-
-servidor.get('/receitas', () => {
-    return receitas
 })
 
 servidor.listen({
